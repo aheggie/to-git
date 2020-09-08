880154484228a1edf8728fda7da60cb97c96c0b1
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-07 23:56:01 -0400

    it's listening

---
+++ bin/index.js
@@ -0,0 +1,14 @@
+const { Command } = require("commander");
+const fs = require("fs");
+
+const program = new Command();
+
+program.version("0.0.1");
+
+program
+  .command("init")
+  .alias("i")
+  .description("initialise to-git on a project (call at the root of a repo)")
+  .action(() => console.log("init"));
+
+program.parse(process.argv);
